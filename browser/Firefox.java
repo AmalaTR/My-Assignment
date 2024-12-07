d6a967f123f3152357db18e9ed6df61ef51156c1
AmalaTR <amala.thiru24@gmail.com>
2024-12-08 02:08:19 +0400

    Add files via upload
    Browser

---
+++ browser/Firefox.java
@@ -0,0 +1,20 @@
+package create.browser;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 100.2f;
+		System.out.println(browserVersion);
+		String browserName = "firefox";
+		System.out.println(browserName);
+		boolean isVisible = true;
+		System.out.println(isVisible);
+		int releaseYear = 1998;
+		System.out.println("Release year = "+releaseYear);
+		char browserLogo = 't';
+		System.out.println("Browser Logo is "+browserLogo);
+
+	}
+
+}
